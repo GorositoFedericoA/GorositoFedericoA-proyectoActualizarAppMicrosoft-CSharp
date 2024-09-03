57526651753a80f121c090bdabddca2e96c6404e
Federico A. Gorosito <federico.a.gorosito@gmail.com>
2024-09-02 21:01:46 -0300

    ,

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74846d51babbaa861e342875039bfcb88da879d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903af807e542a7924b47fff7f71bf9368826156a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
